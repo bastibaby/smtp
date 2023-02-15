691d2f738225dbdca0824eef9e316c3cdd8db206
Héctor Álvarez | MEAT <halvarez@meatcode.cl>
2023-02-15 13:34:33 +0000

    Feat: - Remove css theme, to only use resource css

--- htdocs/content/themes/meat-theme/webpack.mix.js
+++ htdocs/content/themes/meat-theme/webpack.mix.js
@@ -13,5 +13,4 @@ let mix = require('laravel-mix');
 mix.setPublicPath('dist');
 
 mix.js('assets/js/theme.js', 'dist/js/theme.min.js')
-    .sass('assets/sass/style.scss', 'dist/css/theme.css')
     .sass('assets/sass/woocommerce.scss', 'dist/css');
